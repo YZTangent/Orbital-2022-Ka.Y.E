ff39e52d293a82dde23843fe8612fe6525904ac9
wowitzseale <altaria99@gmail.com>
2022-05-30 19:17:35 +0800

    Changed creds folder to a .env file

--- src/hooks/supabaseClient.js
+++ src/hooks/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://vftyplvlxjwndgqvkvxv.supabase.co"
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmdHlwbHZseGp3bmRncXZrdnh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODY3NjUsImV4cCI6MTk2OTE2Mjc2NX0.fC9bnGx6nhTqU-K__yl3OTWYxJ6v5YI1BqCpIpGpv00'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
